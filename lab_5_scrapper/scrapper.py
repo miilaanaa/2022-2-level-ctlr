baefcfe88d4b8054974de807f1bcfdac2e413ae1
milana_hodzhametova <milana.hodzhametova12@gmail.com>
2023-04-20 00:23:16 +0300

    changed

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -302,7 +302,7 @@ class HTMLParser:
             self.article.topics = ', '.join(topics_arr)
 
     @staticmethod
-    def unify_date_format(date_str: str) -> datetime.datetime:
+    def unify_date_format(date_str: str) -> datetime.date:
         """
         Unifies date format
         """
